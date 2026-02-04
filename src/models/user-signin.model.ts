9a9489e40aafa7dde494bcca8da053bd4188f575
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-02-04 17:40:31 -0400

    add model classes for user sign-in and sign-up

---
+++ src/models/user-signin.model.ts
@@ -0,0 +1,17 @@
+import { UserSignInInterface } from '../interfaces/user.interface.js'
+
+export default class UserSignIn implements UserSignInInterface {
+  public email: string
+  public password: string
+
+  constructor(email: string, password: string) {
+    this.email = email
+    this.password = password
+  }
+  getEmail(): string {
+    return this.email
+  }
+  getPassword(): string {
+    return this.password
+  }
+}
---
+++ src/models/user-signup.model.ts
@@ -0,0 +1,27 @@
+import { UserSignupInterface } from '../interfaces/user.interface.js'
+
+export default class UserSignup implements UserSignupInterface {
+  public email: string
+  public password: string
+  public name: string
+  public username: string
+
+  constructor(email: string, pass: string, name: string, username: string) {
+    this.email = email
+    this.password = pass
+    this.name = name
+    this.username = username
+  }
+  getEmail(): string {
+    return this.email
+  }
+  getPassword(): string {
+    return this.password
+  }
+  getUsername(): string {
+    return this.username
+  }
+  getName(): string {
+    return this.name
+  }
+}
