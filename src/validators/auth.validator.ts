3ed0e239e8eccd6a7226d42d2ec9cd28436ab810
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-02-05 08:51:52 -0400

    refactor: simplify signup validation logic

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -7,44 +7,36 @@ import {
 } from '../error/authentication/authError.js'
 
 export default class AuthValidator {
-  private readonly NAME_REGEX = /^[\p{L}\s'-]{2,50}$/u
-  private readonly USERNAME_REGEX = /^[a-zA-Z0-9_]{3,16}$/
-  private readonly EMAIL_REGEX =
-    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  private readonly PASSWORD_REGEX =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  private static readonly RULES = {
+    name: {
+      regex: /^[\p{L}\s'-]{2,50}$/u,
+      error: InvalidNameError,
+    },
+    username: {
+      regex: /^[a-zA-Z0-9_]{3,16}$/,
+      error: InvalidUsernameError,
+    },
+    email: {
+      regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      error: InvalidEmailError,
+    },
+    password: {
+      regex:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      error: InvalidPasswordError,
+    },
+  } as const
 
-  private isValidName(name: string): boolean {
-    if (!this.NAME_REGEX.test(name)) {
-      throw new InvalidNameError(name)
+  private validate(value: string, rule: { regex: RegExp; error: any }) {
+    if (!rule.regex.test(value)) {
+      throw new rule.error(value)
     }
-    return true
-  }
-
-  private isValidUsername(username: string): boolean {
-    return this.USERNAME_REGEX.test(username)
-  }
-
-  private isValidEmail(email: string): boolean {
-    return this.EMAIL_REGEX.test(email)
-  }
-
-  private isValidPassword(password: string): boolean {
-    return this.PASSWORD_REGEX.test(password)
   }
 
   public signUpValidator(user: UserSignupInterface) {
-    if (!this.isValidName(user.getName())) {
-      throw new InvalidNameError(user.getName())
-    }
-    if (!this.isValidEmail(user.getEmail())) {
-      throw new InvalidEmailError(user.getEmail())
-    }
-    if (!this.isValidUsername(user.getUsername())) {
-      throw new InvalidUsernameError(user.getUsername())
-    }
-    if (!this.isValidPassword(user.getPassword())) {
-      throw new InvalidPasswordError(user.getPassword())
-    }
+    this.validate(user.getName(), AuthValidator.RULES.name)
+    this.validate(user.getUsername(), AuthValidator.RULES.username)
+    this.validate(user.getEmail(), AuthValidator.RULES.email)
+    this.validate(user.getPassword(), AuthValidator.RULES.password)
   }
 }
