b2fc26d7036e0b13fe84ee80cd7f55e27b2fa26b
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-02-04 22:54:48 -0400

    refactor: update AuthValidator tests to use UserSignupModel
    - Update imports to use the renamed UserSignupModel
    - Update type annotations and constructor calls in tests

--- src/validators/auth.validator.test.ts
+++ src/validators/auth.validator.test.ts
@@ -1,22 +1,19 @@
-import AuthValidator from './auth.validator' // adjust path
-import { UserSignupInterface } from '../interfaces/user.interface.js'
-import UserSignup from '../models/user-signup.model'
+import AuthValidator from './auth.validator'
+import UserSignupModel from '../models/user-signup.model'
 
 describe('Auth validator test', () => {
   let validator: AuthValidator
-  let user: UserSignup
+  let user: UserSignupModel
   beforeEach(() => {
     validator = new AuthValidator()
   })
   it('should pass if all data is valid', () => {
-    const user = new UserSignup(
+    const user = new UserSignupModel(
       'user@example.com',
       'strongPassworD1@',
       'user',
       'usersName'
     )
-
-    // Testing that it does NOT throw any error
     expect(() => validator.signUpValidator(user)).not.toThrow()
   })
 })
