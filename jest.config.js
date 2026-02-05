9d3e310d68dcbf0cc29ec3709085e474b7cd8225
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-02-04 23:13:00 -0400

    test: configure Jest for ESM and force exit on completion
    - Update jest.config.js to use 'ts-jest/presets/default-esm'
    - Add moduleNameMapper to resolve .js imports to .ts source files
    - Enable useESM in ts-jest transformation settings
    - Add --forceExit flag to test script to handle hanging worker processes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 }
